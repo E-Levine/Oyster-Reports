6ed27a11eb0bc792c60951f797b764a3daf629d2
E-Levine <149699489+E-Levine@users.noreply.github.com>
2025-03-10 19:18:18 -0400

    DBHYDRO updates
    Code corrections, function moved to code file.
    Station reference sheet created.
    CR, LX, SL data updated 1/2023-12/2024.

--- DBHYDRO/DBHYDRO_functions.R
+++ DBHYDRO/DBHYDRO_functions.R
@@ -24,29 +24,40 @@ load_raw_data <- function(file_name){
   return(Raw_data_t)
 }
 #
-#
-#
-###Working
-#
-#
-#working_raw_data <- function(Version_type){
-#  if(Version_type == "New"){
-#    #Read the Excel file
-#    t <- readWorkbook(paste0("Local_data/", Estuary_code, "_", Station, "_", ID, ".xlsx"), sheet = 1, detectDates = TRUE)
-#    #Determine where to start data based on empty row
-#    start_row <- which(t[[1]] == "Station")[1]
-#    if(!is.na(start_row)){
-#      Raw_data <- t[start_row:nrow(t),]
-#    } else {
-#      Raw_data <- t
-#    }
-    #Promote first row to column names, remove columns of NAs, make sure data is only for desired DBKey
-#    Raw_data <- Raw_data %>% slice(-1) %>% setNames(unlist(Raw_data[1,])) %>% 
-#      dplyr::select(where(~ !all(is.na(.)))) %>% filter(DBKEY == ID) 
-    #
-#  } else {
-#    Raw_data <- c("No data found.")
-#  }
-#  return(Raw_data)
-#}
-#
+##Write data if new, append data if additional data for existing file/sheet
+output_shared_data <- function(Shared_file, sheet_name){
+  if(exists("wb")){rm(wb)}
+  #Write data to file based on if New data or adding to existing data
+  if (file.exists(Shared_file)) {
+    print("Data file exists.")
+    # Check if the specified sheet exists
+    if (sheet_name %in% excel_sheets(Shared_file)) {
+      #Load workbook
+      wb <- loadWorkbook(Shared_file)
+      # Get the existing data from the specified sheet, add new data, keep only newest data if duplicates, and write back to same sheet
+      existing_data <- read.xlsx(Shared_file, sheet = sheet_name) %>% 
+        mutate(Age = "Older", Analysis_Date = as.Date(Analysis_Date, origin = "1899-12-30"), Date = as.Date(Date, origin = "1899-12-30"))
+      combined_data <- rbind(existing_data, Cleaned_df %>% mutate(Age = "Newer")) %>% group_by(Date) %>% arrange(Date, Age) %>% slice(1)
+      combined_data <- combined_data %>% dplyr::select(-Age) %>% arrange(Date)
+      writeData(wb, sheet = sheet_name, combined_data)
+      # Save the workbook
+      saveWorkbook(wb, Shared_file, overwrite = TRUE)
+      print(paste0("Sheet exists for station ", sheet_name, ". New data was appended to sheet."))
+    } else {
+      #Load workbook, add worksheet and write data to sheet
+      wb <- loadWorkbook(Shared_file)
+      addWorksheet(wb, sheet_name)
+      writeData(wb, sheet_name, Cleaned_df)
+      saveWorkbook(wb, Shared_file, overwrite = TRUE)
+      print(paste0("Sheet did NOT exist for station ", sheet_name, " so was created."))
+    }
+  } else {
+    # Create a new workbook, add a worksheet and write the data to sheet
+    wb <- createWorkbook()
+    addWorksheet(wb, sheet_name)
+    writeData(wb, sheet_name, Cleaned_df)
+    # Save the workbook to a file
+    saveWorkbook(wb, Shared_file, overwrite = TRUE)
+    print(paste0("Data file did NOT exist so was created. File: ", Estuary_code, "_", Data_type, ".xlsx  Sheet:", sheet_name))
+  }
+}
--- DBHYDRO/Dbhydro_data_cleaning.R
+++ DBHYDRO/Dbhydro_data_cleaning.R
@@ -13,14 +13,13 @@ source("DBHYDRO_Functions.R")
 ##Raw data files should be saved as Excel files in the "Local_data" folder using naming schema:
 #two-letter estuary code, _, station (with special characters removed), _, DBKey: i.e., SL_S80S_DJ238
 #
-#
 ###Set up parameters
 Data_type <- c("Flow") #Current options: "Flow"
-Estuary_code <- c("CR") #two-letter site code
-Site_code <- c("CR") #Is the data attributable to a specific site? If so then include code, otherwise NA
-Station <- c("S79") #station ID with special characters removed
-ID <- c("DJ237")
-Version <- "New" #"New" file or sheet started for the station, or "Adding" new data to an existing file
+Estuary_code <- c("SL") #two-letter site code
+Site_code <- c("SL-S") #Is the data attributable to a specific site? If so then include code, otherwise NA
+Station <- c("S80S") #station ID with special characters removed
+ID <- c("DJ238")
+Version <- "Adding" #"New" file or sheet started for the station, or "Adding" new data to an existing file
 #
 #
 #
@@ -35,7 +34,7 @@ Raw_df %>% filter(is.na(Revision_Date) | !(Revision_Date > Date))
 #
 #
 ##Prepare data for output
-Cleaned_df <- Raw_df %>% dplyr::select(-Revision_Date, -Qualifier) %>% 
+Cleaned_df <- Raw_df %>% dplyr::select(-any_of(c("Revision_Date", "Qualifier"))) %>% 
   rename(!!Data_type := 'Data_Value', Data_Station = "Station") %>%
   mutate(Analysis_Date = as.Date(paste(substr(Date, 1, 4), substr(Date, 6, 7), "15", sep = "-"), format = "%Y-%m-%d"),
          Estuary = Estuary_code,
@@ -44,29 +43,10 @@ Cleaned_df <- Raw_df %>% dplyr::select(-Revision_Date, -Qualifier) %>%
 head(Cleaned_df)
 #
 #
-#Write data to file based on if New data or adding to existing data
-if (file.exists(paste0("Shared_data/", Estuary_code, "_", Data_type,".xlsx"))) {
-  print("Data file exists.")
-  # Check if the specified sheet exists
-  if (Station %in% excel_sheets(paste0("Shared_data/", Estuary_code, "_", Data_type,".xlsx"))) {
-    print(paste0("Sheet exists for station ", Station, "."))
-    } else {
-      #Load workbook
-      wb <- loadWorkbook(paste0("Shared_data/", Estuary_code, "_", Data_type,".xlsx"))
-      addWorksheet(wb, Station)
-      writeData(wb, Station, Cleaned_df)
-      saveWorkbook(wb, paste0("Shared_data/", Estuary_code, "_", Data_type,".xlsx"), overwrite = TRUE)
-      print(paste0("Sheet did NOT exist for station ", Station, " so was created."))
-      }
-  } else {
-  # Create a new workbook
-  wb <- createWorkbook()
-  # Add a worksheet adnd write the data frame to the sheet
-  addWorksheet(wb, Station)
-  writeData(wb, Station, Cleaned_df)
-  # Save the workbook to a file
-  saveWorkbook(wb, paste0("Shared_data/", Estuary_code, "_", Data_type, ".xlsx"), overwrite = TRUE)
-  print(paste0("Data file did NOT exist so was created. File: ", Estuary_code, "_", Data_type, ".xlsx  Sheet:", Station))
-}
+###Write cleaned data to shared file
+#Confirm shared file path
+(shared_file_path <- paste0("Shared_data/", Estuary_code, "_", Data_type,".xlsx"))
+output_shared_data(shared_file_path, Station)
+#
 #
 #
