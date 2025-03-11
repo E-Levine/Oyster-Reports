eeae94e37b0292106e1bb3d18922e322625492df
matthewdavis2985 <matthew.davis@myfwc.com>
2025-03-11 10:52:36 -0400

    Create ReportSetupScript, set report variables

---
+++ DMFM/Annual_Report/scripts/ReportSetup.R
@@ -0,0 +1,24 @@
+# Performs basic set up for running all scripts
+
+# Load necessary R packages
+library(tidyverse)
+library(odbc)
+library(DBI)
+library(dbplyr)
+library(lubridate)
+library(knitr)
+library(ggpubr)
+library(patchwork) #Required for arranging multiple plots, more flexible
+library(scales)
+
+# Set background variables
+ReportStart <- as.Date(paste0(ReportYear, "-01-01"))
+ReportEnd <- as.Date(paste0(ReportYear, "-12-31"))
+Estuaries <- c("AB")
+
+# Configure chunks
+knitr::opts_chunk$set(
+  echo = FALSE,
+  message = FALSE,
+  warning = FALSE
+)
