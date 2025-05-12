4bc8edd56b77aca722c95ffb41cc948166ab0448
E-Levine <149699489+E-Levine@users.noreply.github.com>
2025-05-12 11:42:40 -0400

    Update CERP_annual_data_output.R
    Updated for Cage SH raw data.

--- CERP/CERP_annual_data_output.R
+++ CERP/CERP_annual_data_output.R
@@ -18,7 +18,7 @@ pacman::p_load(odbc, DBI, dbplyr,
                install = TRUE)
 #
 #
-Database <- "Oysters_25-03-31"  #Set the local database to use
+Database <- "Oysters_25-04-25"  #Set the local database to use
 Server = "localhost\\ERICALOCALSQL" #Set the local Server to use
 Estuary_codes <- c("CR", "SL", "LX")
 Annual_year <- c("2024")
@@ -67,6 +67,17 @@ hsdbSrvySH <- tbl(con,in_schema("hsdb", "SurveySH")) %>%
   filter(FixedLocationID %in% dboFixedLocations$FixedLocationID) %>%
   filter(as.Date(substr(QuadratID, 8, 15), format = "%Y%m%d") > as.Date(paste0("01/01/", Annual_year), "%m/%d/%Y") & as.Date(substr(QuadratID, 8, 15), format = "%Y%m%d") < as.Date(paste0("12/31/", Annual_year), "%m/%d/%Y")) %>%
   mutate(SH_n = as.integer(substr(ShellHeightID, 29,31))) %>% filter(SH_n < 11)
+#
+#Cage
+hsdbCage <- tbl(con,in_schema("hsdb", "CageCount")) %>%
+  collect() %>% mutate(FixedLocationID = substring(SampleEventID, 19, 22)) %>% #Create FixedLocationID column and filter to matching IDs
+  filter(FixedLocationID %in% dboFixedLocations$FixedLocationID) %>%
+  filter(as.Date(substr(SampleEventID, 8, 15), format = "%Y%m%d") > as.Date(paste0("01/01/", Annual_year), "%m/%d/%Y") & as.Date(substr(SampleEventID, 8, 15), format = "%Y%m%d") < as.Date(paste0("12/31/", Annual_year), "%m/%d/%Y"))
+hsdbCageSH <- tbl(con,in_schema("hsdb", "CageSH")) %>%
+  collect() %>% mutate(FixedLocationID = substring(CageCountID, 19, 22)) %>% #Create FixedLocationID column and filter to matching IDs
+  filter(FixedLocationID %in% dboFixedLocations$FixedLocationID) %>%
+  filter(as.Date(substr(CageCountID, 8, 15), format = "%Y%m%d") > as.Date(paste0("01/01/", Annual_year), "%m/%d/%Y") & as.Date(substr(CageCountID, 8, 15), format = "%Y%m%d") < as.Date(paste0("12/31/", Annual_year), "%m/%d/%Y")) %>%
+  mutate(SH_n = as.integer(substr(ShellHeightID, 30,32)))
 #
 DBI::dbDisconnect(con)
 #
@@ -177,6 +188,21 @@ SrvySH_df_t <- hsdbSrvySH %>% left_join(dboFixedLocations %>% dplyr::select(Fixe
 SrvySH_df <- SrvySH_df_t %>% anti_join(SrvySH_df_t %>% filter((Season == 'Sum' & Site == "CR-W" & Station == 4) | (Season == 'Sum' & Site == "CR-E" & Station == 1) | (Season == 'Win' & Site == "CR-E" & Station == 1) | (Season == 'Win' & Site == "CR-W" & Station == 4))) %>%
   arrange(SurveyDate, Site, Station, Quadrat)
 #
+Cage_df <- hsdbCage %>% dplyr::select(CageCountID, DeployedDate, RetrievedDate, DaysDeployed, CageColor, FixedLocationID) %>% 
+  left_join(dboFixedLocations %>% dplyr::select(FixedLocationID, Estuary, SectionName, StationNumber)) %>% 
+  mutate(CageCountID = paste0(substring(CageCountID, 1, 22), "_", substring(CageCountID, 28, 28))) %>%
+  mutate(AnalysisDateRet = as.Date(paste0(substr(CageCountID, 12, 13), "/01/", substr(CageCountID, 8, 11)), "%m/%d/%Y"),
+         Month = as.numeric(substr(CageCountID, 12, 13)),
+         Year = as.numeric(substr(CageCountID, 8, 11)),
+         RetJulian = as.numeric(as.Date(substr(CageCountID, 8, 15), "%Y%m%d")-as.Date("2005-01-01")+1),
+         DepJulian = as.numeric(as.Date(DeployedDate, "%Y-%m-%d")-as.Date("2005-01-01")+1),
+         Site = paste0(Estuary, "-", SectionName),
+         SiteSta = paste(Site, StationNumber, sep = " ")) %>%
+  full_join(hsdbCageSH %>% dplyr::select(ShellHeightID, CageCountID, ShellHeight, SH_n) %>% mutate(Type = case_when(substring(CageCountID, 26, 26) == "D" ~ "DepSH", TRUE ~ "RetSH")) %>%  mutate(CageCountID = paste0(substring(CageCountID, 1, 22), "_", substring(CageCountID, 28, 28)), ShellHeightID = paste0(substring(ShellHeightID, 1, 22),"_",substring(ShellHeightID, 28, 31))) %>% pivot_wider(values_from = ShellHeight, names_from = Type) %>% arrange(SH_n)) %>%
+  dplyr::select(AnalysisDateRet, Month, Year, "DepDate" = DeployedDate, "RetDate" = RetrievedDate, DepJulian, RetJulian, "Days" = DaysDeployed, Site, SiteSta, "Station"  = StationNumber, "Cage" = CageColor, DepSH, RetSH, ShellHeightID) %>%
+  arrange(AnalysisDateRet, SiteSta, Cage) %>% mutate_at(c("DepSH", "RetSH"), as.numeric) %>% mutate(DepDate = as.Date(DepDate, format = "%Y-%m-%d"), RetDate = as.Date(RetDate, format = "%Y-%m-%d")) %>%
+  distinct()
+#
 Flow_df <- dsFlow %>% 
   mutate(Year = as.numeric(format(Date, "%Y")),
          Month = as.numeric(format(Date, "%m"))) %>%
@@ -189,17 +215,18 @@ Flow_df <- dsFlow %>%
 ####Save all modified data to output####
 #
 #Load each sheet and append new data
-WQ_excel <- readWorkbook("CERP/CERP_data_raw.xlsx", sheet = 'WaterQuality', detectDates = TRUE) %>% mutate(across(contains("Date"), ~as.Date(.x, format = "%Y-%m-%d"))) %>% 
+WQ_excel <- readWorkbook("CERP/CERP_PBC_Raw_Data.xlsx", sheet = 'WaterQuality', detectDates = TRUE) %>% mutate(across(contains("Date"), ~as.Date(.x, format = "%Y-%m-%d"))) %>% 
  # mutate(Time = format((as.POSIXct(Time*86400, origin = "1970-01-01", tz = "UTC")), "%H:%M")) %>% 
   bind_rows(WQ_df)
-Flow_excel <- readWorkbook("CERP/CERP_data_raw.xlsx", sheet = 'Flow', detectDates = TRUE) %>% mutate(across(contains("Date"), ~as.Date(.x, format = "%Y-%m-%d"))) %>% bind_rows(Flow_df)
-Rcrt_excel <- readWorkbook("CERP/CERP_data_raw.xlsx", sheet = 'Rcrt', detectDates = TRUE) %>% mutate(across(contains("Date"), ~as.Date(.x, format = "%Y-%m-%d"))) %>% bind_rows(Rcrt_df)
-Dermo_excel <- readWorkbook("CERP/CERP_data_raw.xlsx", sheet = 'Dermo', detectDates = TRUE) %>% mutate(across(contains("Date"), ~as.Date(.x, format = "%Y-%m-%d"))) %>% bind_rows(Dermo_df)
-Survey_excel <- readWorkbook("CERP/CERP_data_raw.xlsx", sheet = 'SurveyCounts', detectDates = TRUE) %>% mutate(across(contains("Date"))) %>% bind_rows(Srvy_df)
-SH_excel <- readWorkbook("CERP/CERP_data_raw.xlsx", sheet = 'SurveySHs', detectDates = TRUE) %>% mutate(across(contains("Date"), ~as.Date(.x, format = "%Y-%m-%d"))) %>% bind_rows(SrvySH_df)
+Flow_excel <- readWorkbook("CERP/CERP_PBC_Raw_Data.xlsx", sheet = 'Flow', detectDates = TRUE) %>% mutate(across(contains("Date"), ~as.Date(.x, format = "%Y-%m-%d"))) %>% bind_rows(Flow_df)
+Rcrt_excel <- readWorkbook("CERP/CERP_PBC_Raw_Data.xlsx", sheet = 'Rcrt', detectDates = TRUE) %>% mutate(across(contains("Date"), ~as.Date(.x, format = "%Y-%m-%d"))) %>% bind_rows(Rcrt_df)
+Dermo_excel <- readWorkbook("CERP/CERP_PBC_Raw_Data.xlsx", sheet = 'Dermo', detectDates = TRUE) %>% mutate(across(contains("Date"), ~as.Date(.x, format = "%Y-%m-%d"))) %>% bind_rows(Dermo_df)
+Survey_excel <- readWorkbook("CERP/CERP_PBC_Raw_Data.xlsx", sheet = 'SurveyCounts', detectDates = TRUE) %>% mutate(across(contains("Date"))) %>% bind_rows(Srvy_df)
+SH_excel <- readWorkbook("CERP/CERP_PBC_Raw_Data.xlsx", sheet = 'SurveySHs', detectDates = TRUE) %>% mutate(across(contains("Date"), ~as.Date(.x, format = "%Y-%m-%d"))) %>% bind_rows(SrvySH_df)
+Growth_excel <- readWorkbook("CERP/CERP_PBC_Raw_Data.xlsx", sheet = 'Growth', detectDates = TRUE) %>% mutate(across(contains("Date"), ~as.Date(.x, format = "%Y-%m-%d"))) %>% bind_rows(Cage_df)
 
 #Load workbook of existing data
-Data_wb <- loadWorkbook("CERP/CERP_data_raw.xlsx")
+Data_wb <- loadWorkbook("CERP/CERP_PBC_Raw_Data.xlsx")
 #Overwrite each sheet with new data appended to old
 writeData(Data_wb, "WaterQuality", WQ_excel)
 writeData(Data_wb, "Flow", Flow_excel)
@@ -207,6 +234,7 @@ writeData(Data_wb, "Rcrt", Rcrt_excel)
 writeData(Data_wb, "Dermo", Dermo_excel)
 writeData(Data_wb, "SurveyCounts", Survey_excel)
 writeData(Data_wb, "SurveySHs", SH_excel)
+writeData(Data_wb, "Growth", Growth_excel)
 
 #Save workbook
-saveWorkbook(Data_wb, "CERP/CERP_data_raw.xlsx", overwrite=TRUE)
+saveWorkbook(Data_wb, "CERP/CERP_PBC_Raw_Data.xlsx", overwrite=TRUE)
