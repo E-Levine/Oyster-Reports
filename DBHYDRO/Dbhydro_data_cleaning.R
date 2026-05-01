c9b5932bf2cbe09faaa3964feabf3086c5b9f949
ELevine <149699489+E-Levine@users.noreply.github.com>
2026-05-01 06:00:05 -0400

    46 cerp annual conversion (#66)
    * DBHydro updates
    
    Data file updates.
    Code updates for new Insights downloads.
    
    * Update Annual Report Information.Rmd
    
    Code for Salinity and Secchi R Markdown and Excel file summaries.
    
    * Update Annual Report Information.Rmd
    
    Code for Temp and pH data summaries
    
    * Update Annual Report Information.Rmd
    
    DO summary code.
    
    * Update Annual Report Information.Rmd
    
    Survey data summaries.
    
    * Update Annual Report Information.Rmd
    
    Survey data updates.
    
    * Annual summary output
    
    Code for dermo data summaries.
    
    * Update Annual Report Information.Rmd
    
    Repro data summary code
    
    * Update Annual Report Information.Rmd
    
    Recruitment summary code
    
    * Update Annual Report Information.Rmd
    
    Cage summary code.
    
    * March monthly
    
    March monthly report updates
    
    * Flow data
    
    Updates with historic data
    
    * Update Annual Report Information.Rmd
    
    Added flow data.
    
    * Annual analyses
    
    R markdown files for CERP annual analyses.
    
    * Update Annual Report Analyses Setup.Rmd
    
    Code cleanup.
    Rcrt coding
    Salinity coding
    
    * Markdown updates
    
    File naming updates.
    Survey data filtering updates.
    Analyses coding for survey data.
    
    * Update Annual Report Analyses Setup.Rmd
    
    Updated data setup and usage.
    
    * Update Annual Report Analyses.Rmd
    
    Updated data usage.
    Code for dermo analyses.
    
    * Update Annual Report Analyses.Rmd
    
    Analysis correction.
    
    * Update Annual Report Analyses Setup.Rmd
    
    Analysis corrections.
    
    * A5 files
    
    Files for A5 CERP 2025
    
    * Annual updates
    
    Updates to code and data files for CERP 2026 annual (A6).
    
    * Update site names, data files, and project ID
    
    Correct site naming and update data files: change figure captions and plot labels to use "Caloosahatchee River-" prefixes and replace Bird Island with Kitchel Key where applicable; correct St. Lucie cage station from Station 1 to Station 2 in Rmds; update Stations_cages mapping. Replace/refresh multiple Excel data files (CERP_2026.xlsx, CERP_PBC_Summary_Data.xlsx, CERP_PBC_WQ_Summary_Data.xlsx) and remove temporary ~$.xlsx. Add ProjectId to Quarterly.Rproj. These edits standardize location names and incorporate updated data.
    
    * Refactor file loading for survey data
    
    * Replace Excel file links with placeholders
    
    Updated links in the CERP Monthly Report to use placeholders.
    
    * Monthly and quarterly updates
    
    Updated code files for Apr 2026 monthly and Jan-Mar 2026 quarterly.
    Added Monthly and Quarterly SOP files to repo.
    
    * Annual Report
    
    Minor updates for submission.

--- DBHYDRO/Dbhydro_data_cleaning.R
+++ DBHYDRO/Dbhydro_data_cleaning.R
@@ -13,47 +13,71 @@ source("DBHYDRO_Functions.R")
 ##Raw data files should be saved as Excel files in the "Local_data" folder using naming schema:
 #two-letter estuary code, _, station (with special characters removed), _, DBKey: i.e., SL_S80S_DJ238
 #
-###Set up parameters
+##### Set up parameters ----
 Data_type <- c("Flow") #Current options: "Flow"
-Estuary_code <- c("SL") #two-letter site code
-Site_code <- c("SL-N") #Is the data attributable to a specific site? If so then include code, otherwise NA
-Station <- c("Gordy") #station ID with special characters removed
-ID <- c("91295")
+Estuary_code <- c("LX") #two-letter site code
+Site_code <- c(NA) #Is the data attributable to a specific site? If so then include code, otherwise (or if unsure) NA
+Station_name <- c("Gordy") #station ID with special characters removed
 Version <- "Adding" #"New" file or sheet started for the station, or "Adding" new data to an existing file
 #
+Stations <- read_xlsx("Shared_data/DBHYDRO_stations.xlsx")
+# Filter to possible stations based on parameters set above. 
+#Check and update/filter more as needed to assign ID
+(Stations_f <- Stations %>%
+    filter(Data_Type == Data_type &
+           Estuary == Estuary_code) %>%
+    filter(if (!is.na(Site_code)) {
+      Site == Site_code
+    } else {
+      Station == Station_name
+    }
+    ))
+#
+(LoggerID <- Stations_f$DBKey)
+if(is.na(Site_code)){
+  (Site_code <- Stations_f$Site)
+}
 #
 #
-#####Load new data
+##### Load new data ----
 #
 #Check the the file name matches "filename"
-(filename <- paste0("Local_data/", Estuary_code, "_", Station, "_", ID, ".xlsx"))
+(filename <- paste0("Local_data/", Estuary_code, "_", Station_name, "_", LoggerID, ".xlsx"))
 #
-Raw_df <- load_raw_data(filename)
+Raw_df <- load_raw_data(file_name = filename, ID = LoggerID)
 #Check if any data has not been checked/revised - 0 rows = good
 Raw_df %>% filter(is.na(Revision_Date) | !(Revision_Date > Date))
 #
 #
-##Prepare data for output
-Cleaned_df <- Raw_df %>% dplyr::select(-any_of(c("Revision_Date", "Qualifier"))) %>% 
-  rename(!!Data_type := 'Data_Value', Data_Station = "Station") %>%
+## Prepare data for output
+Cleaned_df <- Raw_df %>% 
+  # Remove revision date, keep qualifier to overwrite as needed
+  dplyr::select(-any_of(c("Revision_Date", "Qualifier"))) %>% 
+  # Rename value col to Data type, Station
+  rename(!!Data_type := 'Data_Value', 
+         Data_Station = any_of(c("Station", "STATION")),
+         DBKEY = any_of(c("DBKEY", "TIMESERIESID"))) %>%
   mutate(Analysis_Date = as.Date(paste(substr(Date, 1, 4), substr(Date, 6, 7), "15", sep = "-"), format = "%Y-%m-%d"),
          Estuary = Estuary_code,
-         Site = Site_code, .before = Data_Station)
-#Check data makes sense
+         Site = Site_code, .before = Data_Station) %>%
+  dplyr::select(Analysis_Date, Estuary, Site, Data_Station, DBKEY, Date, !!Data_type)
+# Check data makes sense
 head(Cleaned_df)
+# 
 #
 #
 #
 #
-#######Write cleaned data to shared file
-#Confirm shared file path
+##### Write cleaned data to shared file ----
+# Confirm shared file path
 (shared_file_path <- paste0("Shared_data/", Estuary_code, "_", Data_type,".xlsx"))
-output_shared_data(shared_file_path, Station)
+# Write data. If adding, any date overlaps are over written, assumes more recently downloaded data has been checked/is more accurate
+output_shared_data(shared_file_path, Station_name)
 #
 #
 #
 #
-####Summarize data if needed
+##### Summarize data if needed (SLE, LOX) ----
 #Can be run after data has been cleaned and added to the shared folder. Does not currently run from local files. 
 ##Can NOT be run without existing data file.
 #Continues from code above or can be run starting here.
@@ -62,9 +86,9 @@ output_shared_data(shared_file_path, Station)
 ##Check all possible sheets and dates available:
 date_range_check(shared_file_path)
 ##Use table to select stations by name and date range:
-Summarize_stations <- c("S80S", "S97S", "S49S", "Gordy") #List of all stations
-Date_range <- c(as.Date("2024-01-01"), as.Date("2024-12-31")) #Min and max dates inclusive
-Site_sum_name <- c("SLESum") #Column name for summary data
+Summarize_stations <- c("LOX", "S46") #List of all stations
+Date_range <- c(as.Date("2024-12-01"), as.Date("2025-12-31")) #Min and max dates inclusive
+Site_sum_name <- c("LOXSum") #Column name for summary data
 #
 data_summary_output(Summarize_stations, Date_range, Site_sum_name)
 #
